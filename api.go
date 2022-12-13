49a3349659491a15212a0d624911c0860882435b
Paul Cager <paul.cager@gmail.com>
2022-12-13 18:29:23 +0000

    Do not expose the result code: todo fail if result != 0

--- api.go
+++ api.go
@@ -46,53 +46,47 @@ func (session *Session) Invalidate() {
 }
 
 type DeviceInfo struct {
-	Result struct {
-		DeviceID           string  `json:"device_id"`
-		FwVer              string  `json:"fw_ver"`
-		HwVer              string  `json:"hw_ver"`
-		Type               string  `json:"type"`
-		Model              string  `json:"model"`
-		Mac                string  `json:"mac"`
-		HwID               string  `json:"hw_id"`
-		FwID               string  `json:"fw_id"`
-		OemID              string  `json:"oem_id"`
-		IP                 string  `json:"ip"`
-		TimeDiff           int     `json:"time_diff"`
-		Ssid               string  `json:"ssid"`
-		Rssi               int     `json:"rssi"`
-		SignalLevel        int     `json:"signal_level"`
-		Latitude           float64 `json:"latitude"`
-		Longitude          float64 `json:"longitude"`
-		Lang               string  `json:"lang"`
-		Avatar             string  `json:"avatar"`
-		Region             string  `json:"region"`
-		Specs              string  `json:"specs"`
-		Nickname           string  `json:"nickname"`
-		HasSetLocationInfo bool    `json:"has_set_location_info"`
-		DeviceOn           bool    `json:"device_on"`
-		OnTime             float64 `json:"on_time"`
-		DefaultStates      struct {
-			Type  string `json:"type"`
-			State struct {
-			} `json:"state"`
-		} `json:"default_states"`
-		Overheated            bool   `json:"overheated"`
-		PowerProtectionStatus string `json:"power_protection_status"`
-	} `json:"result"`
-	ErrorCode int `json:"error_code"`
+	DeviceID           string  `json:"device_id"`
+	FwVer              string  `json:"fw_ver"`
+	HwVer              string  `json:"hw_ver"`
+	Type               string  `json:"type"`
+	Model              string  `json:"model"`
+	Mac                string  `json:"mac"`
+	HwID               string  `json:"hw_id"`
+	FwID               string  `json:"fw_id"`
+	OemID              string  `json:"oem_id"`
+	IP                 string  `json:"ip"`
+	TimeDiff           int     `json:"time_diff"`
+	Ssid               string  `json:"ssid"`
+	Rssi               int     `json:"rssi"`
+	SignalLevel        int     `json:"signal_level"`
+	Latitude           float64 `json:"latitude"`
+	Longitude          float64 `json:"longitude"`
+	Lang               string  `json:"lang"`
+	Avatar             string  `json:"avatar"`
+	Region             string  `json:"region"`
+	Specs              string  `json:"specs"`
+	Nickname           string  `json:"nickname"`
+	HasSetLocationInfo bool    `json:"has_set_location_info"`
+	DeviceOn           bool    `json:"device_on"`
+	OnTime             float64 `json:"on_time"`
+	DefaultStates      struct {
+		Type  string `json:"type"`
+		State struct {
+		} `json:"state"`
+	} `json:"default_states"`
+	Overheated            bool   `json:"overheated"`
+	PowerProtectionStatus string `json:"power_protection_status"`
 }
 
 type EnergyUsage struct {
-	Result struct {
-		TodayRuntimeMins       int    `json:"today_runtime"`
-		MonthRuntimeMins       int    `json:"month_runtime"`
-		TodayEnergyWattHours   int    `json:"today_energy"`
-		MonthEnergyWattHours   int    `json:"month_energy"`
-		LocalTime              string `json:"local_time"`
-		ElectricityCharge      []int  `json:"electricity_charge"`
-		CurrentPowerMilliWatts int    `json:"current_power"`
-	} `json:"result"`
-	ErrorCode int `json:"error_code"`
+	TodayRuntimeMins       int    `json:"today_runtime"`
+	MonthRuntimeMins       int    `json:"month_runtime"`
+	TodayEnergyWattHours   int    `json:"today_energy"`
+	MonthEnergyWattHours   int    `json:"month_energy"`
+	LocalTime              string `json:"local_time"`
+	ElectricityCharge      []int  `json:"electricity_charge"`
+	CurrentPowerMilliWatts int    `json:"current_power"`
 }
 
 func (session *Session) Switch(on bool) error {
@@ -108,15 +102,23 @@ func (session *Session) Switch(on bool) error {
 }
 
 func (session *Session) GetDeviceInfo() (*DeviceInfo, error) {
-	info := new(DeviceInfo)
-	err := session.Post(message{Method: "get_device_info"}, info)
-	return info, err
+	resp := struct {
+		Result    DeviceInfo `json:"result"`
+		ErrorCode int        `json:"error_code"`
+	}{}
+
+	err := session.Post(message{Method: "get_device_info"}, &resp)
+	return &resp.Result, err
 }
 
 func (session *Session) GetEnergyUsage() (*EnergyUsage, error) {
-	usage := new(EnergyUsage)
-	err := session.Post(message{Method: "get_energy_usage"}, &usage)
-	return usage, err
+	resp := struct {
+		Result    EnergyUsage `json:"result"`
+		ErrorCode int         `json:"error_code"`
+	}{}
+
+	err := session.Post(message{Method: "get_energy_usage"}, &resp)
+	return &resp.Result, err
 }
 
 func (session *Session) Post(body interface{}, response interface{}) error {
